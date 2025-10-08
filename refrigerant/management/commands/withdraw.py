952706553a5d78747a2c6767b4dc35c177992a72
Jesse De Loore <jesse@sennac.be>
2025-10-08 14:43:59 +0200

    fix race condition and improve feedback in simulation

--- refrigerant/management/commands/withdraw.py
+++ refrigerant/management/commands/withdraw.py
@@ -6,25 +6,35 @@ import threading
 class Command(BaseCommand):
     help = "Simulate condition when withdrawing refrigerant from a vessel."
 
-    def handle(self, *args, **kwargs):
-        Vessel.objects.create(name="Test Vessel", content=50.0)
-        self.stdout.write("Simulating condition...")
-        self.run_simulation()
+    def add_arguments(self, parser):
+        parser.add_argument("withdraw_amount", nargs="?", type=int, default=10)
 
-    def run_simulation(self):
+    def handle(self, *args, **kwargs):
+        vessel = Vessel.objects.create(name="Test Vessel", content=50.0)
+        self.stdout.write("Simulating condition...")
+        self.run_simulation(vessel.id, kwargs["withdraw_amount"])
+
+    def run_simulation(self, vessel_id:int, amount:int):
         barrier = threading.Barrier(2)
 
         def user1():
             barrier.wait()
-            vessel = Vessel.objects.get(id=1)
-            vessel.content -= 10.0
-            vessel.save()
+            if (new_content := Vessel.withdraw(vessel_id=vessel_id, amount=amount)) == 1:
+                self.stdout.write(f"Successfully withdrawn {amount}kg refrigerant.")
+            elif new_content == 0:
+                self.stdout.write(f"Vessel with id={vessel_id} not found in the database.")
+            else:
+                self.stdout.write("Not enough content to withdraw 10 kg")
 
         def user2():
             barrier.wait()
-            vessel = Vessel.objects.get(id=1)
-            vessel.content -= 10.0
-            vessel.save()
+            if (new_content := Vessel.withdraw(vessel_id=vessel_id, amount=amount)) == 1:
+                self.stdout.write(f"Successfully withdrawn {amount}kg refrigerant.")
+            elif new_content == 0:
+                self.stdout.write(f"Vessel with id={vessel_id} not found in the database.")
+            else:
+                self.stdout.write("Not enough content to withdraw 10 kg")
+
 
         t1 = threading.Thread(target=user1)
         t2 = threading.Thread(target=user2)
@@ -33,5 +43,6 @@ class Command(BaseCommand):
         t1.join()
         t2.join()
 
-        vessel = Vessel.objects.get(id=1)
+
+        vessel = Vessel.objects.get(id=vessel_id)
         self.stdout.write(f"Remaining content: {vessel.content} kg")
--- refrigerant/models.py
+++ refrigerant/models.py
@@ -1,5 +1,5 @@
-from django.db import models
-from django.core.validators import MinValueValidator
+from django.db import models, DataError, IntegrityError
+from django.db.models import F
 
 
 class Vessel(models.Model):
@@ -7,3 +7,18 @@ class Vessel(models.Model):
     content = (
         models.PositiveIntegerField()
     )  # Amount of refrigerant in the vessel, in kilograms
+
+    @classmethod
+    def withdraw(cls, vessel_id: int, amount: int)->int|None:
+        """
+        Do an atomic update of the vessel content
+        Returns 1 if the vessel was updated
+        Returns 0 if no row was found to update
+        Returns None if there is not enough content in the vessel to withdraw the amount requested.
+        """
+        try:
+            # Once updated to Django>=5 we can leerage the UPDATE ... RETURNING ... syntax to return the amount left in the vessel.
+            return cls.objects.filter(id=vessel_id).update(content=F("content") - amount)
+        except (DataError, IntegrityError):
+            return None
+
